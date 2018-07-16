4a57a1c0e4657a1db93b525a4adc4916946be862
David Casanellas <davidcasanell@gmail.com>
2018-07-16 19:12:22 +0200

    feat: import HttpClientModule and ApiService in app.component and display posts using subscribe function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  postsSubscription$: Subscription;
+  posts: any;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.postsSubscription$ = this.apiService
+      .getPosts()
+      .subscribe((res: Response) => (this.posts = res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { ApiService } from './api.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostsComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, PostsComponent],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
