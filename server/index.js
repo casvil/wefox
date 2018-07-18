c5709d1f513c0b18d8345bca0caff3f5c54b8bd8
David Casanellas <davidcasanell@gmail.com>
2018-07-18 13:53:46 +0200

    fix: static filepath

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/../dist/WeFox'));
+app.use(express.static('../dist/WeFox'));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/../dist/WeFox/index.html'));
+  res.sendFile(path.join('../dist/WeFox/index.html'));
 });
 
 app.listen(process.env.PORT || 8080); // Default Heroku port
